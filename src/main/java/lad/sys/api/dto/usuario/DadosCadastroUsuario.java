dccbdca16e603cf7e0391c12839e083a26d79fa9
Gabriel Martins <gabrielmartinscp@hotmail.com>
2026-05-19 09:35:01 -0300

    Implementação da funcionalidade de cadastro de usuário

--- src/main/java/lad/sys/api/dto/usuario/DadosCadastroUsuario.java
+++ src/main/java/lad/sys/api/dto/usuario/DadosCadastroUsuario.java
@@ -3,6 +3,7 @@ package lad.sys.api.dto.usuario;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 public record DadosCadastroUsuario(
 
@@ -10,7 +11,7 @@ public record DadosCadastroUsuario(
         String nome,
 
         @NotBlank
-        @Pattern(regexp = "\\c{8,30}")
+        @Size(min = 3, max = 50)
         String senha,
 
         @Email
